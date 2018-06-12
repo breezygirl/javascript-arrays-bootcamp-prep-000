ad12c7546ca6a925e25523c990e012d56c6a612e
Beerscheba C <designergo@aol.com>
2018-06-12 20:32:19 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,45 @@
+const expect = ('chai');
+const fs = ('fs');
+const jsdom =('mocha-jsdom');
+const path = ('path');
+
+
+var  chocolateBars= [ 'snickers', 'hundred grand', 'kitkat','skittles'];
+
+function  addElementToBeginningOfArray (array, element){
+  return [element,...array]
+  }
+ 
+function destructivelyAddElementToBeginningOfArray(array, element){
+array.unshift(element)
+  return array 
+}
+ 
+ function addElementToEndOfArray(array, element){
+ 
+var array2= array.concat(element)
+return array2
+   
+ }
+
+  
+  function destructivelyAddElementToEndOfArray(array, element){
+    array.push(element)
+    return array
+  }
+
+function accessElementInArray( array, index)
+{ console.log(accessElementInArray[3])
+}
+
+
+function destructivelyRemoveElementFromBeginningOfArray(array=[1,2,3]){ 
+  
+array.shift()
+return 1
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array=[1,2,3]){
+  array= array.slice(1)
+ return array 
+}
