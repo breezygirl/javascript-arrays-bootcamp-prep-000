e216fac2b3a6c93f36c92938e1c71228570a4300
Beerscheba C <designergo@aol.com>
2018-06-12 21:10:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -66,6 +66,7 @@ return array
   
   function removeElementFromEndOfArray(array=[1,2,3]){
     array.slice(0, array.length - 1) 
-    return 1, 2
+    console.log(array)
+    return array
     
   }
